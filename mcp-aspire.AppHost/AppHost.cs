c171ca6b22e3df31cd27dcd22263850cf34144ce
Mike Kistler <mikekistler@microsoft.com>
2025-07-31 10:35:57 -0700

    Delete Web App

--- mcp-aspire.AppHost/AppHost.cs
+++ mcp-aspire.AppHost/AppHost.cs
@@ -3,10 +3,4 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.mcp_aspire_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.mcp_aspire_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
 builder.Build().Run();
